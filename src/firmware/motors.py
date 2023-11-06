07281ae41bbb2552714ca44f85818c85f4fe742c
NinjaPro93 <ninjapro93lu@gmail.com>
2023-11-06 18:32:58 -0300

    deixei a motors mais tunada

--- src/firmware/motors.py
+++ src/firmware/motors.py
@@ -10,41 +10,51 @@ class ESC:
     MIN_WIDTH = 650
     MAX_WIDTH = 2400
 
-    def __init__(self, pin:int)-> None:
+    def __init__(self, pin1:int, pin2:int = None)-> None:
         self.conn = pigpio.pi()
-        self.pin = pin
+        self.pin1 = pin1
+        self.pin2 = pin2
         
-        self.conn.set_mode(self.pin, pigpio.OUTPUT)  #pigpio.PWM(12)  
-        #pi.set_servo_pulsewidth(ESC1_GPIO, 0)
-        self.conn.set_PWM_frequency(self.pin, 50)
+        self.conn.set_mode(self.pin1, pigpio.OUTPUT)  #pigpio.PWM(12)  
+        
+        self.conn.set_PWM_frequency(self.pin1, 50)
+        
+        if self.pin2 != None:
+            self.conn.set_mode(self.pin2, pigpio.OUTPUT)
+            self.conn.set_PWM_frequency(self.pin2, 50)
 
 
+    def pwm(self, width: int)-> None:
+        self.conn.set_servo_pulsewidth(self.pin1, width)
+        if self.pin2 != None:
+            self.conn.set_servo_pulsewidth(self.pin2, width)
+        
 
     def calibrate(self)-> None:
         print('"menor" velocidade')
-        self.conn.set_servo_pulsewidth(self.pin, self.MIN_WIDTH)
+        self.pwm(self.MIN_WIDTH)
         time.sleep(5)
         
         print('"maior" velocidade')
-        self.conn.set_servo_pulsewidth(self.pin, self.MAX_WIDTH)
+        self.pwm(self.MAX_WIDTH)
         time.sleep(5)
         
         print("calibrado pai")
-        self.conn.set_servo_pulsewidth(self.pin, 0)
+        self.pwm(self.MIN_WIDTH)
         
     def arm(self)-> None:
         print("armando")
-        self.conn.set_servo_pulsewidth(self.pin, self.MIN_WIDTH)
+        self.pwm(self.MIN_WIDTH)
         time.sleep(2)
         
         print("armado")
         
     def halt(self)-> None:
         print("parando")
-        self.conn.set_servo_pulsewidth(self.pin, self.MIN_WIDTH)
+        self.pwm(self.MIN_WIDTH)
         
         print("ta safe")
-        self.conn.set_servo_pulsewidth(self.pin, 0)
+        self.pwm(0)
         
         print("desligando GPIO.")
         self.conn.stop()
@@ -53,40 +63,44 @@ class ESC:
     
     def control(self)-> None:
         a = input()
-        veloc = self.conn.get_servo_pulsewidth(self.pin)
+        veloc = self.conn.get_servo_pulsewidth(self.pin1)
         
         while a != 'x':
             if a == 'q':
                 veloc += 10
-                self.conn.set_servo_pulsewidth(self.pin, veloc)
+                self.pwm(veloc)
             
             if a == 'a':
                 veloc -= 10
-                self.conn.set_servo_pulsewidth(self.pin, veloc)
+                self.pwm(veloc)
             
             if a == 'e':
                 veloc += 100
-                self.conn.set_servo_pulsewidth(self.pin, veloc)
+                self.pwm(veloc)
             
             if a == 'd':
                 veloc -= 100
-                self.conn.set_servo_pulsewidth(self.pin, veloc)
+                self.pwm(veloc)
             
+            print(veloc)
             a = input()
     
     def test(self) ->None:
-        self.conn.set_servo_pulsewidth(self.pin, self.MIN_WIDTH)
+        self.pwm(self.MIN_WIDTH)
         
         step = 100
         print("acelerando")
         for veloc in range(self.MIN_WIDTH, self.MAX_WIDTH, step):
-            self.conn.set_servo_pulsewidth(self.pin, veloc)
+            self.pwm(veloc)
+            print(veloc)
             time.sleep(1)
         
         time.sleep(2)  
         print("parando")
         for veloc in range(self.MAX_WIDTH, self.MIN_WIDTH, -step):
-            self.conn.set_servo_pulsewidth(self.pin, veloc)
+            print(veloc)
+            self.pwm(veloc)
+            time.sleep(.5)  
                 
 
 class Servo:
@@ -114,6 +128,7 @@ class Servo:
         while width != "x":
             while width < self.MIN_WIDTH or width > self.MAX_WIDTH:
                 width = input("ta fazendo merda: ")    
+                
             self.conn.set_servo_pulsewidth(self.pin, width)
             width = int(input('angulo novo: '))
         
@@ -124,33 +139,31 @@ if __name__ == "__main__":
 
     servo = Servo(pin=18)
     
-    esc1 = ESC(pin=12)
-    esc2 = ESC(pin=13)
+    escs = ESC(pin1=12, pin2=13)
     while True:
         inp = input("vai mexer onde?")
     
         if inp == "esc":
-            print("vou callibrar as paradas e testar")
-            esc1.calibrate()
-            esc2.calibrate()
+            #print("vou callibrar as paradas e testar")
+            #esc1.calibrate()
             
-            esc1.arm()
-            esc2.arm()
+            escs.arm()
             
-            esc1.test()
-            esc2.test()
+            #esc1.test()
+            #esc2.test()
             
             sOUn = input("quer brincar?")
             if sOUn == 's':
-                esc1.control()
+                escs.control()
             
         elif inp == "servo":
-            print("vou callibrar a parada e testar")
-            servo.test()
+            #print("vou callibrar a parada e testar")
+            #servo.test()
             
             sOUn = input("quer brincar?")
             if sOUn == 's':
                 servo.control()
             
         elif inp == "x":
+            escs.halt()
             break
